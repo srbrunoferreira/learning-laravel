1c309871fadc8696c530b76fa0a956aff9b46137
Bruno Ferreira <srbrunoferreira@outlook.com>
2021-07-15 08:00:49 -0300

    Aula 9

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = 'Matheus';
+    return view('welcome', ['nome' => $nome]);
+});
+
+Route::get('/contact/', function() {
+    return view('contact');
+});
+
+Route::get('/produtos', function() {
+    $busca = request('search');
+    return view('products', ['busca' => $busca]);
+});
+
+Route::get('/produtos_test/{id?}', function($id = null) {
+    return view('products', ['id' => $id]);
 });
