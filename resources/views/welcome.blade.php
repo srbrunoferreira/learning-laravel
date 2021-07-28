0264647b2050cfcba73bea5e56ce5d7d4498b368
Bruno Ferreira <srbrunoferreira@outlook.com>
2021-07-28 08:54:48 -0300

    Home page style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,28 @@
 
 @section('content')
 
-@foreach($events as $event)
-  <p>{{ $event->title }} -- {{ $event->description }}</p>
-@endforeach
+<div id="search-container" class="col-md-12">
+  <h1>Busque um evento</h1>
+  <form action="" method="post">
+    <input type="search" id="search" class="form-control" placeholder="Buscar...">
+  </form>
+</div>
+<div id="events-container" class="col-md-12">
+  <h2>Próximos eventos</h2>
+  <p class="subtitle">Veja os eventos dos próximos dias</p>
+  <div id="cards-container" class="row">
+    @foreach($events as $event)
+      <div class="card col-md-3">
+        <img src="img/event_placeholder.jpg" alt="{{ $event->title }}">
+        <div class="card-body">
+          <p class="card-date">10/09/2020</p>
+          <h5 class="card-title">{{ $event->title }}</h5>
+          <p class="card-participants">X Participantes</p>
+          <a href="#" class="btn btn-primary">Saber mais</a>
+        </div>
+      </div>
+    @endforeach
+  </div>
+</div>
 
 @endsection
